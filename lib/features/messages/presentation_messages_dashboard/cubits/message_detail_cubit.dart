4de922cd3f3e8a2ed45c55f8bdef1dbf9d3e4f76
cristianmgm7 <cristianmgm7@gmail.com>
2025-12-04 23:10:42 -0500

    refactor: Simplify MessageDetailCubit and integrate MessageDetailBloc
    - Removed the dependency on MessageDetailBloc from MessageDetailCubit, streamlining its functionality.
    - Updated DashboardScreen to listen for changes in MessageDetailCubit and trigger LoadMessageDetail events accordingly.
    - Adjusted the positioning logic in DashboardContent for improved UI responsiveness when the composition panel is open.

--- lib/features/messages/presentation_messages_dashboard/cubits/message_detail_cubit.dart
+++ lib/features/messages/presentation_messages_dashboard/cubits/message_detail_cubit.dart
@@ -1,25 +1,18 @@
 import 'package:carbon_voice_console/features/messages/presentation_messages_dashboard/cubits/message_detail_state.dart';
-import 'package:carbon_voice_console/features/messages/presentation_messages_detail/bloc/message_detail_bloc.dart'
-    as detail_bloc;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:logger/logger.dart';
 
 @injectable
 class MessageDetailCubit extends Cubit<MessageDetailState> {
-  MessageDetailCubit(this._logger, this._messageDetailBloc)
-      : super(const MessageDetailState());
+  MessageDetailCubit(this._logger) : super(const MessageDetailState());
 
   final Logger _logger;
-  final detail_bloc.MessageDetailBloc _messageDetailBloc;
 
   /// Open detail panel for a message
   void openDetail(String messageId) {
     _logger.i('Opening detail panel for message: $messageId');
     emit(MessageDetailState(selectedMessageId: messageId));
-
-    // Trigger the MessageDetailBloc to load the message
-    _messageDetailBloc.add(detail_bloc.LoadMessageDetail(messageId));
   }
 
   /// Close detail panel
